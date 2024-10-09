d42276435518763e6cf4da7fff853b1f511675a9
Mathis <mathiscrochet@gmail.com>
2024-10-09 14:33:03 +0200

    Responsive for ConditionPage

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import NavBar from './components/NavBar.vue';
     <router-view />
   </main>
 
-  <footer class="bottom-0 left-0 z-20 w-full p-4 mt-12 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-500">
+  <footer class="bottom-0 left-0 z-20 w-full p-4 mt-12 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <RouterLink to="/" class="hover:underline me-4 md:me-6">HealthLink™</RouterLink>Tous droits réservés.</span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
